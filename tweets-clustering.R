244d550fe7b798e1298222f0259872df1e8a7459
H-Finch <vijay.somanath@outlook.com>
2016-04-25 23:54:11 +0530

    Update tweets-clustering.R

--- tweets-clustering.R
+++ tweets-clustering.R
@@ -17,6 +17,9 @@ myCorpus <- tm_map(myCorpus, content_transformer(tolower))
 removeURL <- function(x) gsub("http[^[:space:]]*", "", x)
 # tm v0.6
 myCorpus <- tm_map(myCorpus, content_transformer(removeURL))
+#remove hashtags
+removeURL <- function(x) gsub("#(\\d|\\w)+", "", x)
+myCorpus <- tm_map(myCorpus, content_transformer(removeURL))
 # tm v0.5-10
 # myCorpus <- tm_map(myCorpus, removeURL)
 # remove anything other than English letters or space
@@ -27,7 +30,7 @@ myCorpus <- tm_map(myCorpus, content_transformer(removeNumPunct))
 # remove numbers
 # myCorpus <- tm_map(myCorpus, removeNumbers)
 # add two extra stop words: "available" and "via"
-myStopwords <- c(stopwords('english'), "available", "via")
+#myStopwords <- c(stopwords('english'), "available", "via")
 # remove "r" and "big" from stopwords
 myStopwords <- setdiff(myStopwords, c("r", "big"))
 # remove stopwords from corpus
@@ -70,3 +73,54 @@ tdm
 #frequent words & associations
 idx <- which(dimnames(tdm)$Terms == "floods")
 inspect(tdm[idx + (0:5), 101:110])
+# inspect frequent words
+(freq.terms <- findFreqTerms(tdm, lowfreq = 15))
+term.freq <- rowSums(as.matrix(tdm))
+term.freq <- subset(term.freq, term.freq >= 15)
+df <- data.frame(term = names(term.freq), freq = term.freq)
+library(ggplot2)
+ggplot(df, aes(x = term, y = freq)) + geom_bar(stat = "identity") +
+  xlab("Terms") + ylab("Count") + coord_flip()
+# which words are associated with 'flood'?
+findAssocs(tdm, "flood", 0.25)
+
+source("http://bioconductor.org/biocLite.R")
+biocLite("graph")
+biocLite("Rgraphviz")
+
+plot(tdm, term = freq.terms, corThreshold = 0.1, weighting = T)
+
+# remove sparse terms
+tdm2 <- removeSparseTerms(tdm, sparse = 0.95)
+m2 <- as.matrix(tdm2)
+# cluster terms
+distMatrix <- dist(scale(m2))
+fit <- hclust(distMatrix, method = "ward.D")
+plot(fit)
+rect.hclust(fit, k = 10) # cut tree into 6 clusters
+
+m3 <- t(m2) # transpose the matrix to cluster documents (tweets)
+set.seed(122) # set a fixed random seed
+k <- 6 # number of clusters
+kmeansResult <- kmeans(m3, k)
+round(kmeansResult$centers, digits = 3) # cluster centers
+
+for (i in 1:k) {
+cat(paste("cluster ", i, ": ", sep = ""))
+s <- sort(kmeansResult$centers[i, ], decreasing = T)
+cat(names(s)[1:5], "nn")
+# print the tweets of every cluster
+# print(tweets[which(kmeansResult$cluster==i)])
+}
+
+dtm <- as.DocumentTermMatrix(tdm)
+library(topicmodels)
+lda <- LDA(dtm, k = 4) # find 8 topics
+(term <- terms(lda, 6)) # first 6 terms of every topic
+term <- apply(term, MARGIN = 2, paste, collapse = ", ")
+
+# first topic identified for every document (tweet)
+topic <- topics(lda, 1)
+topics <- data.frame(date=as.IDate(tweets.df$created), topic)
+qplot(date, ..count.., data=topics, geom="density",
+      fill=term[topic], position="stack")
