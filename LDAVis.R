7f358dd6b6b62e6063ed5ec5ffc1717e0ae8f854
H-Finch <vijay.somanath@outlook.com>
2016-05-04 23:59:35 +0530

    Added files via upload

---
+++ LDAVis.R
@@ -0,0 +1,85 @@
+#####LATENT DIRICHLET ALLOCATION#####
+text=dat$text
+text = function(x) gsub("'", "", x)  # remove apostrophes
+text = function(x) gsub("[[:punct:]]", " ", x)  # replace punctuation with space
+text = function(x) gsub("[[:cntrl:]]", " ", x)  # replace control characters with space
+text = function(x) gsub("^[[:space:]]+", "", x) # remove whitespace at beginning of documents
+text = function(x) gsub("[[:space:]]+$", "", x) # remove whitespace at end of documents
+text = function(x) gsub("http\\w+", "", x)
+text = function(x) gsub("pic\\w+", "", x)
+text = function(x) gsub("twitter\\w+", "", x)
+text = function(x) gsub("#(\\d|\\w)+", "", x)
+text = function(x) gsub("@\\w+", "", x)
+text = function(x) tolower(x) # force to lowercase
+tdm=removeSparseTerms(tdm,0.99)
+dim(tdm)
+tdm = tdm[rowSums(as.matrix(tdm))>0,]
+SpecificTerms = function(lda.model,n=1) {
+  p = posterior(lda.model)$terms
+  n = min(n,ncol(p))
+  cumulativep = colSums(p)
+  specificp = t(apply(p,1,function(x) ((x) + (x/cumulativep) )))
+  
+  topterms = t(apply(specificp, 1, function(x)
+    (colnames(specificp)[order(x,decreasing=T)[1:n]]) ))
+  
+  topterms
+}
+set.seed(51)
+trainpoints = sample(1:nrow(dtm),1.0*nrow(dtm),replace=F) # to train on a subsample, change 1.0 to a lower value, say 0.8
+k = 6
+lda.model = LDA(dtm[trainpoints,], k)
+
+t(SpecificTerms(lda.model,10))
+terms(lda.model,100)#final result
+
+save(lda.model,file="results/lda.model")
+require(reshape2)
+require(ggplot2)
+require(RColorBrewer)
+
+lda.topics = topics(lda.model,1)
+termgenerator = posterior(lda.model)$terms
+
+###1: relative probabilities of words in each topic ###
+termimportance = apply(termgenerator,1,
+                        function(x)	x[order(x,decreasing=T)[1:100]])
+termimportance.longform = melt(termimportance,
+                                value.name="probability",
+                                varnames=c("termnumber","topic"))
+
+ggplot(data=termimportance.longform,
+       aes(
+         x=termnumber,
+         y=probability,
+         color=factor(topic),
+         group=topic)) + 
+  geom_line()
+###2: Individual comics' distribution over topics ###
+comics.topics = posterior(lda.model,dtm[trainpoints,])$topics
+df.comics.topics = as.data.frame(comics.topics)
+df.comics.topics = cbind(comic=as.character(rownames(df.comics.topics)),
+                          df.comics.topics, stringsAsFactors=F)
+
+df.comics.topics.longform = melt(df.comics.topics,
+                                  id.vars="comic",variable.name="topic")
+df.comics.topics.longform = df.comics.topics.longform[order(as.numeric(df.comics.topics.longform$comic)),]
+
+topic.names = terms(lda.model,6)
+topic.names = apply(topic.names, 2, paste, collapse=",")
+names(topic.names) = NULL
+
+n.comics.to.plot = 6000
+start.comic = 1
+
+four.colour.palette = brewer.pal(10,"Paired")[c(1,3,5,7)]
+
+bplot = ggplot(df.comics.topics.longform[(start.comic*k)+1:(n.comics.to.plot*k),],
+                aes(x=comic,y=value)) + 
+  geom_bar(stat="identity",position="stack",aes(fill=topic))
+
+bplot + 
+  scale_fill_manual(values=four.colour.palette,
+                    name="Topic", breaks=1:k,labels=topic.names) +
+  coord_flip()
+
